054e3dfdec707ed03648ff27bc3adbde9f13e9d3
lzy <liangzhaoyuan21@qq.com>
2024-02-03 22:53:14 +0800

    feature：暂定添加一个全局的tookit包和Base64工具类

---
+++ template/src/main/java/com/cy/toolkit/Base64Util.java
@@ -0,0 +1,32 @@
+package com.cy.toolkit;
+
+/**
+ * Base64Util Base64工具类
+ *
+ * @author Liang Zhaoyuan
+ * @version 2024/02/02 19:19
+ **/
+public class Base64Util {
+
+
+    /**
+     * 将图片文件转换成base64字符串，参数为该图片的路径
+     *
+     * @param imageFile
+     * @return java.lang.String
+     */
+    public String ImageToBase64(String imageFile) {
+        return null;
+    }
+
+    /**
+     * 将base64解码成图片并保存在传入的路径下
+     * 第一个参数为base64 ，第二个参数为路径
+     *
+     * @param base64, imgFilePath
+     * @return boolean
+     */
+    public boolean Base64ToImage(String base64, String imgFilePath) {
+        return false;
+    }
+}
