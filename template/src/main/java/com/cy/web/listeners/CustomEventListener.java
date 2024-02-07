1526d36383d7688ef60ffc065da95e1d3fa27bba
lzy <liangzhaoyuan21@qq.com>
2024-02-07 22:20:12 +0800

    update：完善afterNavigateTo

--- template/src/main/java/com/cy/web/listeners/CustomEventListener.java
+++ template/src/main/java/com/cy/web/listeners/CustomEventListener.java
@@ -1,5 +1,6 @@
 package com.cy.web.listeners;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
  * @author Liang Zhaoyuan
  * @version 2024/02/07 21:58
  **/
+@Slf4j
 public class CustomEventListener extends AbstractWebDriverEventListener {
     /**
      * 模拟正常浏览器访问户行为，隐藏 navigator.webdriver 属性
@@ -18,7 +20,13 @@ public class CustomEventListener extends AbstractWebDriverEventListener {
      */
     @Override
     public void afterNavigateTo(String url, WebDriver driver) {
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        jsExecutor.executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+        try {
+            log.info("afterNavigateTo: {}", url);
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            jsExecutor.executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+        } catch (Exception e) {
+            log.warn("隐藏 navigator.webdriver 属性 出现异常: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 }
