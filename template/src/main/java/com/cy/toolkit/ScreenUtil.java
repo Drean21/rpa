10d032a28e1733c909223d432aa3f09006c2aebb
lzy <liangzhaoyuan21@qq.com>
2024-02-12 12:38:25 +0800

    feature：增加ScreenUtil

---
+++ template/src/main/java/com/cy/toolkit/ScreenUtil.java
@@ -0,0 +1,24 @@
+package com.cy.toolkit;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+
+public class ScreenUtil {
+
+    /**
+     * 从剪切板获得图片。
+     */
+    private static Image getImageFromClipboard() throws Exception {
+        Clipboard sysc = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Thread.sleep(150);
+        Transferable cc = sysc.getContents(null);
+        if (cc == null) {
+            return null;
+        } else if (cc.isDataFlavorSupported(DataFlavor.imageFlavor)) {
+            return (Image) cc.getTransferData(DataFlavor.imageFlavor);
+        }
+        return null;
+    }
+}
